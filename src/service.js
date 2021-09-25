145473167a2e5f5c77dfa4e7a2d9370451168e98
ivan <ivan.nikolicamp@gmail.com>
2021-09-25 03:08:50 +0200

    cors

--- src/service.js
+++ src/service.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 const KEY = '43a4d7553f56245b96fc070d6b777809'
 // c27280d79ef611f61af7059d96f7f614
-const ALL_TECHNOLOGY_NEWS = `https://api.mediastack.com/v1/news?access_key=${KEY}&categories=technology`
-const ALL_SPORT_NEWS = `https://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports`
+const ALL_TECHNOLOGY_NEWS = `http://api.mediastack.com/v1/news?access_key=${KEY}&categories=technology`
+const ALL_SPORT_NEWS = `http://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports`
 
-const TOP_NEWS_EN = `https://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports, technology&languages= -de,es,fr,it,en,nl`
-const TOP_NEWS_ALL = `https://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports, technology&country= en `
+const TOP_NEWS_EN = `http://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports, technology&languages= -de,es,fr,it,en,nl`
+const TOP_NEWS_ALL = `http://api.mediastack.com/v1/news?access_key=${KEY}&categories=sports, technology&country= en `
 
 
 
